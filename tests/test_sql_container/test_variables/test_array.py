805dc66a407bbf175705251672328a51ba702a8e
akvilary <abc-sm@yandex.ru>
2023-08-05 16:04:52 +0800

    add tests

--- tests/test_sql_container/test_variables/test_array.py
+++ tests/test_sql_container/test_variables/test_array.py
@@ -4,7 +4,23 @@ from sqlconstructor import SqlContainer
 
 
 @pytest.mark.SqlContainer
-def test_array_variable():
-    my_list = ['a', 'b']
-    container = SqlContainer('$my_list')(my_list=my_list)
+@pytest.mark.parametrize("my_array", [
+    [1, 2],
+    (1, 2),
+    {1, 2},
+])
+def test_array_variable(my_array):
+    container = SqlContainer('$my_array')(my_array=my_array)
+    assert container.dumps() == "ARRAY[1, 2]"
+
+
+def test_array_variable_strings_case_single_quotes():
+    my_array = ['a', 'b']
+    container = SqlContainer('$my_array')(my_array=my_array)
+    assert container.dumps() == "ARRAY['a', 'b']"
+
+
+def test_array_variable_strings_case_double_quotes():
+    my_array = ["a", "b"]
+    container = SqlContainer('$my_array')(my_array=my_array)
     assert container.dumps() == "ARRAY['a', 'b']"
