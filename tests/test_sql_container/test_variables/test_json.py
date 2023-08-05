f72322a0d8065efa3b246fc1ad7223477f2227eb
akvilary <abc-sm@yandex.ru>
2023-08-05 16:04:52 +0800

    add 1.2.7 version

--- tests/test_sql_container/test_variables/test_json.py
+++ tests/test_sql_container/test_variables/test_json.py
@@ -1,10 +1,12 @@
 import pytest
 import uuid
 
-from sqlconstructor import SqlContainer
+from sqlconstructor import SqlContainer, SqlVal, SqlCol
 
 
 @pytest.mark.SqlContainer
+@pytest.mark.SqlVal
+@pytest.mark.SqlCol
 def test_json_variable():
     uuid_value = uuid.uuid4()
     my_dict = {
@@ -13,6 +15,9 @@ def test_json_variable():
         'tuple': ('a', 'b'),
         'set': {1, 2},
         1: uuid_value,
+        'container': SqlContainer('a'),
+        'val': SqlVal('a'),
+        'col': SqlCol('a'),
     }
     container = SqlContainer("SELECT $my_dict::json->'tuple' as names")(my_dict=my_dict)
     assert container.dumps() == (
@@ -21,6 +26,9 @@ def test_json_variable():
         '"list": ["a", "b"], '
         '"tuple": ["a", "b"], '
         '"set": [1, 2], '
-        f'"1": "{uuid_value}"'
+        f'"1": "{uuid_value}", '
+        '"container": "a", '
+        '"val": "\'a\'", '
+        '"col": "\\\"a\\\""'
         "}::json->'tuple' as names"
     )
