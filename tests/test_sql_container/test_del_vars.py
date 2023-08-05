4a2dda28f3f48b2708a2d2a709637e67617ed8c1
akvilary <abc-sm@yandex.ru>
2023-08-05 16:04:52 +0800

    add pytest and write tests

---
+++ tests/test_sql_container/test_del_vars.py
@@ -0,0 +1,29 @@
+import pytest
+from sqlconstructor import SqlContainer
+
+
+@pytest.mark.SqlContainer
+def test_del_all_vars_of_container():
+    sql_text = 'quality = $quality AND brand_id = $brand_id'
+    container = SqlContainer(sql_text)
+    assert len(container.vars) == 0
+    container(quality='Best', brand_id=1)
+    assert len(container.vars) == 2
+
+    container.vars.clear()
+    assert len(container.vars) == 0
+    assert container.dumps() == sql_text
+
+
+@pytest.mark.SqlContainer
+def test_del_certain_var_of_container():
+    sql_text = 'quality = $quality AND brand_id = $brand_id'
+    container = SqlContainer(sql_text)
+    assert len(container.vars) == 0
+    container(quality='Best', brand_id=1)
+    assert len(container.vars) == 2
+
+    del container.vars['quality']
+    assert len(container.vars) == 1
+    assert '$quality' in container.dumps()
+    assert '$brand_id' not in container.dumps()
--- tests/test_sql_container/test_set_vars.py
+++ tests/test_sql_container/test_set_vars.py
@@ -20,27 +20,42 @@ def test_building_query_with_variables(simple_query_sql):
         'AND brand_id = $brand_id',
     )(quality='Best', brand_id=1)
     container = q()
-    assert container.dumps() == simple_query_sql
+    make_common_assertions(container, simple_query_sql)
 
 
 @pytest.mark.SqlQuery
 @pytest.mark.SqlSection
 @pytest.mark.SqlContainer
 def test_set_variables_by_calling_container_after_building_query(
-    simple_query_dict_with_placeholders, simple_query_sql
+    simple_query_dict_with_placeholders,
+    simple_query_sql,
 ):
     q = SqlQuery(simple_query_dict_with_placeholders)
     container = q()
+    assert len(container.vars) == 0
     container(quality='Best', brand_id=1)
-    assert container.dumps() == simple_query_sql
+    make_common_assertions(container, simple_query_sql)
 
 
 @pytest.mark.SqlQuery
 @pytest.mark.SqlSection
 @pytest.mark.SqlContainer
-def test_set_variables_by_vars_attribute_after_building_query(simple_query_dict_with_placeholders, simple_query_sql):
+def test_set_variables_by_vars_attribute_after_building_query(
+    simple_query_dict_with_placeholders,
+    simple_query_sql,
+):
     q = SqlQuery(simple_query_dict_with_placeholders)
     container = q()
+    assert len(container.vars) == 0
     container.vars['quality'] = 'Best'
     container.vars['brand_id'] = 1
+    make_common_assertions(container, simple_query_sql)
+
+
+def make_common_assertions(container, simple_query_sql):
+    assert len(container.vars) == 2
+    # check that in str representation placeholders are not replaced by vars
+    assert 'Best' not in str(container)
+    assert '1' not in str(container)
+    # check that placeholders are replaced by vars only in 'dumps' method result
     assert container.dumps() == simple_query_sql
