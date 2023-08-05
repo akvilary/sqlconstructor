864966292c5b98c76627ad60481a7a1e3f1a8bee
akvilary <abc-sm@yandex.ru>
2023-08-05 16:04:52 +0800

    add 1.0.19 version

--- tests/test_sql_container/test_set_vars.py
+++ tests/test_sql_container/test_set_vars.py
@@ -25,18 +25,20 @@ def test_set_variables_by_vars_attribute():
     sql_text = 'quality = $quality AND brand_id = $brand_id'
     container = SqlContainer(sql_text)
     assert len(container.vars) == 0
-    container.vars['quality'] ='Best'
+    container.vars['quality'] = 'Best'
     container.vars['brand_id'] = 1
     make_common_assertions(container)
 
 
-def make_common_assertions(container, type_casting = False):
+def make_common_assertions(container, type_casting=False):
     assert len(container.vars) == 2
     # check that in str representation placeholders are not replaced by vars
-    assert 'Best' not in str(container)
-    assert '1' not in str(container)
+    assert (
+        str(container) == f"quality = $quality{'::text' if type_casting else ''}"
+        f" AND brand_id = $brand_id{'::int' if type_casting else ''}"
+    )
     # check that placeholders are replaced by vars only in 'dumps' method result
-    if type_casting:
-        assert container.dumps() == "quality = 'Best'::text AND brand_id = 1::int"
-    else:
-        assert container.dumps() == "quality = 'Best' AND brand_id = 1"
+    assert (
+        container.dumps() == f"quality = 'Best'{'::text' if type_casting else ''}"
+        f" AND brand_id = 1{'::int' if type_casting else ''}"
+    )
