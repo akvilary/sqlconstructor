e48b3c260cd53bede7771c5343f62b4911b54d57
akvilary <abc-sm@yandex.ru>
2023-08-05 16:04:52 +0800

    add pytest and write tests

---
+++ tests/test_sql_query/test_add.py
@@ -0,0 +1,27 @@
+import pytest
+from sqlconstructor import SqlQuery
+from fixtures.expected_examples import select_section_of_simple_query_sql
+
+
+@pytest.mark.SqlQuery
+@pytest.mark.SqlSection
+@pytest.mark.SqlContainer
+def test_add_simple():
+    q = SqlQuery()
+    q.add('abc xyz')
+    container = q()
+    assert str(container) == 'abc xyz'
+
+
+@pytest.mark.SqlQuery
+@pytest.mark.SqlSection
+@pytest.mark.SqlContainer
+def test_add_simple_to_filled_query(select_section_of_simple_query_sql):
+    q = SqlQuery()
+    q['select'](
+        'id',
+        'name',
+    )
+    q.add('abc xyz')
+    container = q()
+    assert str(container) == (select_section_of_simple_query_sql + '\nabc xyz')
