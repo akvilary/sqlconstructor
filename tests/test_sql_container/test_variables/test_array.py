a7bd633ba228aee517907cf2fa0e8ebb7750414a
akvilary <abc-sm@yandex.ru>
2023-08-05 16:04:52 +0800

    add tests

--- tests/test_sql_container/test_variables/test_array.py
+++ tests/test_sql_container/test_variables/test_array.py
@@ -6,5 +6,5 @@ from sqlconstructor import SqlContainer
 @pytest.mark.SqlContainer
 def test_array_variable():
     my_list = ['a', 'b']
-    container = SqlContainer("$my_list")(my_list=my_list)
+    container = SqlContainer('$my_list')(my_list=my_list)
     assert container.dumps() == "ARRAY['a', 'b']"
